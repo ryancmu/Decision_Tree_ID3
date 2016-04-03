d8f29a04cad1d124449fef9c201381cb6206a700
ryancmu <zji1@andrew.cmu.edu>
2016-04-03 03:50:10 -0400

    Add the TreeNode Class

---
+++ ID3_Java/src/TreeNode.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+/**
+ * Created by ryan on 4/3/16.
+ *
+ * This class is used to define the struture of the node
+ *
+ */
+public class TreeNode {
+    // The attribute that this node stand for
+    private String attribute;
+
+    // The children nodes of this node
+    private Map<String, TreeNode> children;
+
+    // The result label of this node
+    private String result;
+
+
+
+}
