6a8ea103fea76fd85cb4c5c041ec5184e0e7d6dc
ryancmu <zji1@andrew.cmu.edu>
2016-04-03 15:42:28 -0400

    Add some useful references

--- ID3_Java/src/DecisionTree.java
+++ ID3_Java/src/DecisionTree.java
@@ -2,9 +2,40 @@
  * Created by ryan on 4/2/16.
  *
  * Some References: http://www.cnblogs.com/zhangchaoyang/articles/2196631.html
+ *                  http://czhsuccess.iteye.com/blog/1864652
+ *                  https://github.com/charlottelinxue/C4.5-Decision-Tree-Implementation
  *
  *
  */
 public class DecisionTree {
 
+    // Input Process
+    private void processInput(){
+
+    }
+
+    // Calculate the Information Entropy
+    private double CalcIE(){
+        return 0;
+    }
+
+    // Calculate the Information Gain
+    private double CalcIG(){
+        return 0;
+    }
+
+    // Build the decision tree
+    private void buildTree(){
+
+    }
+
+    // Cross Validation
+    private void crossValidate(){
+
+    }
+
+    // Predict the test set
+    private void predictTestData(){
+
+    }
 }
